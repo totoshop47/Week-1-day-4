71c962c78aeefed0b08c5ae86c82e71e11a9f6eb
Jimyung Park <pzy51992@hotmail.com>
2018-11-30 03:48:55 +0000

    fake roll of die

---
+++ makeLoadedDie.js
@@ -0,0 +1,45 @@
+// var rollDie = function () {
+//   return Math.floor(1 + Math.random() * 6);
+// }
+
+// console.log(rollDie());
+
+
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var index = -1;
+
+
+  return function() {
+
+    index += 1;
+    return list[index];
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
+
+// function makeIdGenerator() {
+//   var id = 0;
+
+//   // The following is the closure function
+//   return function() {
+//     // This inner function accesses and assigns the value of
+//     // the variable id, which was defined in the parent function
+//     id += 1;
+//     return id;
+//   }
+// }
+
+// // makeIdGenerator returns a function which is assigned to
+// // the variable nextId
+// var nextId = makeIdGenerator();
+
+// console.log(nextId()); // Logs: 1
+// console.log(nextId()); // Logs: 2
+// console.log(nextId()); // Logs: 3
